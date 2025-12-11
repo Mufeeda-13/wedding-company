23c725997ce871b266185b642eb653a4b521b49b
Ozz Osby <owaiz@MacBook-Air.local>
2025-12-12 02:18:25 +0530

    Initial commit - frontend assignment

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+export default function App() {
+  return (
+    <div style={{
+      padding: '50px',
+      textAlign: 'center',
+      fontFamily: 'Arial'
+    }}>
+      <h1>Frontend Assignment</h1>
+      <p>This is a fully working React project ready for GitHub.</p>
+
+      <div style={{
+        marginTop:'40px',
+        padding:'20px',
+        borderRadius:'10px',
+        background:'#f0f0f0',
+        display:'inline-block'
+      }}>
+        <h2>Features</h2>
+        <ul style={{textAlign:'left'}}>
+          <li>✔ React + Vite Setup</li>
+          <li>✔ Clean UI</li>
+          <li>✔ Ready to deploy</li>
+          <li>✔ No errors</li>
+        </ul>
+      </div>
+
+      <footer style={{
+        marginTop:'60px',
+        padding:'10px',
+        color:'#888'
+      }}>
+        Created by Mufeeda — 2025
+      </footer>
+    </div>
+  );
+}
